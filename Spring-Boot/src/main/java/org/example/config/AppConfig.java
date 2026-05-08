24a27b6bee1ebb8e6ca119b27f2b3b01e892f200
Egor-git-cloud <egor20061312345@gmail.com>
2026-05-08 23:37:14 +0300

    Add files via upload

---
+++ Spring-Boot/src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,28 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan; 
+
+@Configuration
+@ComponentScan("org.example")
+public class AppConfig {
+    // Никаких @Bean!!
+}
+
+// public class AppConfig {
+    
+//     @Bean
+//     public MessageService messageService() {
+//         return new SmsService(); 
+//     }
+
+//     @Bean
+//     public HeaderService headerService() {
+//         return new HeaderService();
+//     }
+
+//     @Bean
+//     public NotificationManager notificationManager(MessageService ms, HeaderService hs) {
+//         return new NotificationManager(ms, hs);
+//     }
+// }
